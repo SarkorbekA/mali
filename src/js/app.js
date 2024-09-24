dff8c7b12c968b260d9f79756c789b25fe284dd3
Sarkor <abilkosimovsarkor@gmail.com>
2024-09-24 21:15:35 +0500

    feat: complete responsibility

--- src/js/app.js
+++ src/js/app.js
@@ -11,10 +11,20 @@ var swiper = new Swiper(".mySwiper", {
   },
   pagination: {
     el: ".swiper-pagination.mySwiperPagination",
+    clickable: true, 
   },
   effect: "fade",
   loop: true,
   modules: [Navigation, Pagination, EffectFade]
+});
+
+var swiper5 = new Swiper(".swiperFeatures", {
+  pagination: {
+    el: ".swiper-pagination.facts-pagination",
+    clickable: true, 
+  },
+  slidesPerView: 1.3,
+  modules: [Pagination]
 });
 
 var swiper1 = new Swiper(".productSwiper", {
@@ -24,14 +34,21 @@ var swiper1 = new Swiper(".productSwiper", {
   },
   pagination: {
     el: ".swiper-pagination.productSwiperPagination",
+    clickable: true, 
   },
-  slidesPerView: 4,
-  modules: [Navigation, Pagination]
+  slidesPerView: 2,
+  modules: [Navigation, Pagination],
+  breakpoints: {
+    577: {
+      slidesPerView: 4,
+    },
+  },
 });
 
 var swiper3 = new Swiper(".itemSwiper", {
   pagination: {
     el: ".swiper-pagination.itemSwiperPagination",
+    clickable: true, 
   },
   // effect: "fade",
   loop: true,
@@ -39,14 +56,61 @@ var swiper3 = new Swiper(".itemSwiper", {
 });
 
 var swiper4 = new Swiper(".videoSlider", {
+  modules: [Navigation, EffectFade, Pagination],
   navigation: {
     nextEl: ".about__video-next",
     prevEl: ".about__video-prev",
   },
+  pagination: {
+    el: ".videoPagination",
+    clickable: true, 
+  },
   slidesPerView: 1,
   loop: true,
-  modules: [Navigation, EffectFade]
+  spaceBetween: 20,
+  breakpoints: {
+    577: {
+      spaceBetween: 0,
+    },
+  },
 });
+
+var swiper7 = new Swiper(".partnerSwiper", {
+  modules: [Pagination],
+  // navigation: {
+  //   nextEl: ".about__video-next",
+  //   prevEl: ".about__video-prev",
+  // },
+  pagination: {
+    el: ".partnerPagination",
+    clickable: true, 
+  },
+  slidesPerView: 1,
+});
+
+
+var swiper5 = new Swiper(".detailSwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next.detailNav",
+    prevEl: ".swiper-button-prev.detailNav",
+  },
+  pagination: {
+    el: ".swiper-pagination.detailPagination",
+    clickable: true, 
+  },
+  slidesPerView: 1,
+  loop: true,
+  modules: [Navigation, Pagination]
+});
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
 
 
 document.querySelectorAll('.catalog__item-top').forEach(item => {
